79c5271ab0cda4c0f3ea3b9a26fecb8236de1603
Alberto Bojorquez <alberto.bojorquezb@gmail.com>
2020-05-12 09:39:43 -0500

    Ejercicio 1

---
+++ 1.2 - TaxCalculator/TaxCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstTestProject.Services;
+
+namespace FirstTestProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Salario bruto anual:");
+			var grossStr = Console.ReadLine();
+			if (!Decimal.TryParse(grossStr, out decimal gross))
+			{
+				Console.WriteLine("No es un valor válido. Pulsa enter para salir.");
+				Console.ReadLine();
+				return;
+			}
+			var taxService = new TaxService();
+			Console.WriteLine($"Vas a pagar {taxService.GetTax(gross)}.");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ 1.2 - TaxCalculator/TaxCalculator/Services/TaxService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTestProject.Services
+{
+	public class TaxService
+	{
+
+		public decimal GetTax(decimal grossSalary)
+		{
+			if (grossSalary < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(grossSalary));
+			}
+			if (grossSalary < 8000)
+			{
+				return 1000;
+			}
+			if (grossSalary < 20000)
+			{
+				return 2000;
+			}
+			if (grossSalary < 40000)
+			{
+				return 3000;
+			}
+			if (grossSalary < 60000)
+			{
+				return 5000;
+			}
+			return 8000;
+		}
+
+		public string Capitalize(string text)
+		{
+			if (text.Length == 1)
+			{
+				return text.ToUpper();
+			}
+			if (text == String.Empty)
+			{
+				return String.Empty;
+			}
+
+			return text.Substring(0, 1).ToUpper() + text.Substring(1);
+		}
+	}
+}
